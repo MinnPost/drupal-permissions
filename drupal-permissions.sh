d181b4ea7465ac6a8894dd9ab3324b740726f5ac
Alan Palazzolo <apalazzolo@minnpost.com>
2013-05-30 13:39:51 -0500

    Minor update to syntax.

--- drupal-permissions.sh
+++ drupal-permissions.sh
@@ -19,21 +19,21 @@ httpd_group="${3:-www-data}"
 
 # Help menu
 print_help() {
-  cat <<-HELP
-  
-  This script is used to fix permissions of a Drupal installation
-  you need to provide the following arguments:
-  
-  1) Path to your Drupal installation.
-  2) Username of the user that you want to give files/directories ownership.
-  3) HTTPD group name (defaults to www-data for Apache).
-  
-  Usage: (sudo) bash ${0##*/} --drupal_path=PATH --drupal_user=USER --httpd_group=GROUP
-  
-  Example: (sudo) bash ${0##*/} --drupal_path=/usr/local/apache2/htdocs --drupal_user=john --httpd_group=www-data
-  
-  HELP
-  exit 0
+cat <<-HELP
+
+This script is used to fix permissions of a Drupal installation
+you need to provide the following arguments:
+
+1) Path to your Drupal installation.
+2) Username of the user that you want to give files/directories ownership.
+3) HTTPD group name (defaults to www-data for Apache).
+
+Usage: (sudo) bash ${0##*/} --drupal_path=PATH --drupal_user=USER --httpd_group=GROUP
+
+Example: (sudo) bash ${0##*/} --drupal_path=/usr/local/apache2/htdocs --drupal_user=john --httpd_group=www-data
+
+HELP
+exit 0
 }
 
 # Parse Command Line Arguments
